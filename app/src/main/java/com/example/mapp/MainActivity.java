7e90c8970656a8da1c941c29094f5283362407e0
xuanlocgs2000 <xuanlocboya@gmail.com>
2022-09-21 13:46:02 +0700

    1

---
+++ app/src/main/java/com/example/mapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity    {
+
+    private MyMapFragment myMapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        this.myMapFragment = (MyMapFragment) fragmentManager.findFragmentById(R.id.fragment_map);
+
+    }
+
+}
